c34e0eace770c077c161c74175b6b3897b7da0a2
Meteaure Studios <meteaure.studios@gmail.com>
2021-05-30 17:49:08 +0200

    basic sine wave sequencgin

--- app/src/main/cpp/AudioEngine.cpp
+++ app/src/main/cpp/AudioEngine.cpp
@@ -10,12 +10,13 @@
 #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, TAG,__VA_ARGS__)
 #define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, TAG,__VA_ARGS__)
 
+// -------------------------------------------------------------------------------------------------
 void AudioEngine::start()
 {
     oboe::AudioStreamBuilder builder;
 
     builder.setFormat(oboe::AudioFormat::Float);
-    builder.setChannelCount(2);
+    builder.setChannelCount(1);
 
     builder.setSharingMode(oboe::SharingMode::Exclusive);
     builder.setPerformanceMode(oboe::PerformanceMode::LowLatency);
@@ -29,7 +30,10 @@ void AudioEngine::start()
         return;
     }
 
-    mSineWaveGen.setSampleRate(mStream->getSampleRate());
+    // Initialization
+    setSampleRate(mStream->getSampleRate());
+    mEnvelopeBuffer.resize(mStream->getBufferCapacityInFrames());
+    mSineBuffer.resize(mStream->getBufferCapacityInFrames());
 
     result = mStream->requestStart();
 
@@ -38,6 +42,7 @@ void AudioEngine::start()
     }
 }
 
+// -------------------------------------------------------------------------------------------------
 void AudioEngine::stop()
 {
     if(mStream) {
@@ -47,21 +52,57 @@ void AudioEngine::stop()
     }
 }
 
+// -------------------------------------------------------------------------------------------------
+void AudioEngine::setSampleRate(float sampleRate)
+{
+    mSampleRate = sampleRate;
+    mSineWaveGen.setSampleRate(sampleRate);
+    UpdateSequencePhaseIncrement();
+}
+
+// -------------------------------------------------------------------------------------------------
+void AudioEngine::UpdateSequencePhaseIncrement()
+{
+    const double samples_per_step = (60.0/mTempo) * mSampleRate / 4.f; // 1/16 bar
+    mSequencePhaseIncrement = 1.0/samples_per_step;
+}
+
+// -------------------------------------------------------------------------------------------------
 oboe::DataCallbackResult
 AudioEngine::onAudioReady(oboe::AudioStream *oboeStream, void *audioData, int32_t numFrames)
 {
     // Fill buffer with silence
     std::memset(audioData, 0, numFrames*mStream->getChannelCount()*sizeof(float));
 
-    if(mIsActive) {
-        auto buffer = static_cast<float *>(audioData);
-        int channels = mStream->getChannelCount();
-        mSineWaveGen.process(buffer, channels, numFrames);
+    auto buffer = static_cast<float *>(audioData);
+    int channels = mStream->getChannelCount();
+
+    // Sine wave -----
+
+    mSineWaveGen.process(mSineBuffer.data(), channels, numFrames);
+
+    // Sequencer -----
+
+    for(uint32_t i=0; i < numFrames; ++i) {
+
+        // Step change
+        if(mSequencePhase >= 1.0) {
+
+            // Increase step counter
+            mCurrentStep = (mCurrentStep+1) % (kSequencerStepsCount);
+            mSequencePhase = fmod(mSequencePhase, 1.0);
+        }
+
+        const float envelope_level = (float) std::max(1.0 - mSequencePhase, 0.0);
+        buffer[i] = envelope_level * mSineBuffer[i];
+
+        mSequencePhase += mSequencePhaseIncrement;
     }
 
     return oboe::DataCallbackResult::Continue;
 }
 
+// -------------------------------------------------------------------------------------------------
 void AudioEngine::onErrorAfterClose(oboe::AudioStream *oboeStream, oboe::Result error)
 {
     if(error == oboe::Result::ErrorDisconnected) {
@@ -72,10 +113,14 @@ void AudioEngine::onErrorAfterClose(oboe::AudioStream *oboeStream, oboe::Result 
     LOGE("Error was %s", oboe::convertToText(error));
 }
 
-void AudioEngine::setIsActive(bool active) {
+// -------------------------------------------------------------------------------------------------
+void AudioEngine::setIsActive(bool active)
+{
     mIsActive = active;
 }
 
-void AudioEngine::setSineFrequency(float freq) {
+// -------------------------------------------------------------------------------------------------
+void AudioEngine::setSineFrequency(float freq)
+{
     mSineWaveGen.setFrequency(freq);
 }
--- app/src/main/cpp/AudioEngine.h
+++ app/src/main/cpp/AudioEngine.h
@@ -6,6 +6,7 @@
 #define MHS_ANDROID_SESSION_2_AUDIOENGINE_H
 
 #include <Oboe/Oboe.h>
+#include <vector>
 #include "SineWaveGenerator.h"
 
 class AudioEngine
@@ -16,6 +17,7 @@ public:
 
     void start();
     void stop();
+
 
     void setIsActive(bool active);
     void setSineFrequency(float freq);
@@ -28,10 +30,27 @@ public:
 
 private:
 
+    void setSampleRate(float sampleRate);
+    void UpdateSequencePhaseIncrement();
+
+private:
+
     std::shared_ptr<oboe::AudioStream> mStream;
     bool mIsActive = false;
 
     SineWaveGenerator mSineWaveGen;
+
+    float mSampleRate = 0.f;
+
+    // Sequencing related
+    static constexpr int kSequencerStepsCount = 8;
+    double mSequencePhase = 0.f;
+    double mSequencePhaseIncrement = 0.f;
+    float mTempo = 120;
+    int mCurrentStep = 0;
+    std::vector<float> mEnvelopeBuffer;
+    std::vector<float> mSineBuffer;
+
 };
 
 
